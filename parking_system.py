7e68d23022abfe3103c7f7037ecb0c2893f560e9
eddie-m-m <eddie.m.menefee@gmail.com>
2023-12-12 17:25:00 -0800

    First commit

---
+++ parking_system.py
@@ -0,0 +1,19 @@
+# design_parking_system
+
+# implement_encapsulation
+
+# incorporate_polymorphism
+
+# write_parking_system_code
+
+# test_parking_system
+
+'''
+class Parking_system
+class Vehicle
+    def get_type()
+        pass
+class Car
+class Motorcycle
+class Parking_spaces
+'''
